38c7d6fadec8689dd253ea288bec9b417d85b908
kelcisayshello <kelcimensah@gmail.com>
2024-05-10 22:52:08 -0400

    updating module packages

--- src/routes/Studio.tsx
+++ src/routes/Studio.tsx
@@ -29,14 +29,13 @@ function Studio() {
                 <h1>Studio Showcase.</h1>
               </div>
 
-              <p className='route-page-summary'>Explore my studio! This space hosts a growing collection of hobby projects, learning resources, articles, and other technical content.</p>
+              <p className='route-page-summary'>Explore my studio space which hosts a growing collection of side projects, learning resources, articles, and other technical content!</p>
 
               <div className="studio-grid">
                 <Tilt className="s-demos tilt-tile" tiltReverse={true}>
                   <a href="/#/studio/demos">
                     <div className="padding">
                       <h1 className='tile-header'>Demos</h1>
-                      <p>Take a look at my playground of interactive demos and visualizations, showcasing some of my coolest personal projects. They're live and ready for clicks.</p>
                     </div>
                   </a>
                 </Tilt>
@@ -45,7 +44,6 @@ function Studio() {
                   <a href="#">
                     <div className="padding">
                       <h1 className='tile-header'>Articles</h1>
-                      <p>This space is for informative, opinionated, or hobby-interest technical writing pieces. Mostly for me but feel free to take a look!</p>
                     </div>
                   </a>
                 </Tilt>
@@ -54,7 +52,6 @@ function Studio() {
                   <a href="/#/studio/projects">
                     <div className="padding">
                       <h1 className='tile-header'>Projects</h1>
-                      <p>Browse through a collection of personal projects built from college-days to the present. Subject matter topics include front-end design and web development, data structures and algorithms, machine learning, cloud computing, and data science.</p>
                     </div>
                   </a>
                 </Tilt>
